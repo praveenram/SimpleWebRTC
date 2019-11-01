fee670ecb8365169ce3781c4c5e940df4a65f528
Praveenram <praveenram.ba@gmail.com>
2019-10-31 20:50:21 -0400

    Update build script to generate uglified version

--- build.js
+++ build.js
@@ -10,6 +10,9 @@ bundle.bundle(function (err, source) {
     console.error(err);
   }
   fs.writeFileSync('out/simplewebrtc.bundle.js', source);
+  fs.writeFile('out/latest-v2.js', uglify.minify(source.toString('utf8'), {fromString: true}).code, function (err) {
+    if (err) throw err;
+  });
   const adapter = fs.readFileSync('node_modules/webrtc-adapter/out/adapter.js').toString();
   fs.writeFileSync('out/simplewebrtc-with-adapter.bundle.js', `${adapter}\n${source}`);
 });
